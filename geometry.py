c1891072d56efd8a22b5e6cb79f9dd69c21b5a2b
trassis <essemeia@gmail.com>
2024-07-07 11:50:01 -0300

    Mudanças em polygon

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def angle(point1, point2, point3):
+    return True
+
+def in_triangle(point, triangle):
+    return False
--- polygon.py
+++ polygon.py
@@ -1,3 +1,5 @@
+from geometry import angle, in_triangle
+
 class Polygon:
     def __init__(self, file_name = ''):
         self.size = 0
@@ -6,6 +8,7 @@ class Polygon:
         if file_name != '':
             self.read_from_file(file_name)
 
+    # Obtem um poligono representado em um arquivo
     def read_from_file(self, file_name):
         self.points = []
         with open(file_name, 'r') as file:
@@ -13,3 +16,25 @@ class Polygon:
             for _ in range(self.size):
                 x, y = map(int, file.readline().strip().split())
                 self.points.append((x,y))
+
+    # Retorna se o ponto no índice idx é uma orelha
+    def is_ear(self, idx):
+        if self.size < 3:
+            return False
+
+        previous_point = self.points[idx-1]
+        point = self.points[idx]
+        next_point = self.points[idx+1]
+
+        # Verifica se o angulo entre os pontos é à esquerda
+        if angle(previous_point, point, next_point) >= 0:
+            return False
+
+        # Verifica se há algum ponto dentro do triangulo
+        triangle = [ previous_point, point, next_point ]
+        for point in self.points:
+            if in_triangle(point, triangle):
+                return False
+
+        return True
+
